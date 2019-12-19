4ec729e164e22360ce001a1701e0b2d14f9f34f0
galihpermana858 <gpermana858@gmail.com>
2019-12-19 09:31:30 +0700

    JAVA-PERT-13

---
+++ 04.Memahami program/src/pkg04/memahami/program/MemahamiProgram.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pkg04.memahami.program;
+
+/**
+ *
+/**
+ *
+ * @author  NIM   : A2.1900070
+ *          NAMA  : Galih Permana
+ *          KELAS : TI-1A
+ */
+public class MemahamiProgram {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        		System.out.println("hello world");
+    }
+    
+}
---
+++ 35-fungsi void tanpa return/src/com/tutorial/Main.java
@@ -0,0 +1,44 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.tutorial;
+/**
+ *
+ * @author  NIM   : A2.1900070
+ *          NAMA  : Galih Permana
+ *          KELAS : TI-1A
+ */
+
+public class Main {
+
+    public static void main (String[] args){
+
+        // void itu artinya hampa
+
+        System.out.println(simpel());
+
+        fungsiVoid("apa pun");
+        selamatPagi("galih");
+        selamatPagi("nisa");
+
+    }
+
+    private static void selamatPagi(String nama){
+        System.out.println("selamat pagi " + nama);
+    }
+
+
+    // fungsi atau method tanpa kembalian
+    private static void fungsiVoid(String input){
+        System.out.println(input);
+    }
+
+    // fungsi atau method dengan kembalian
+    // sehingga menggunakan return untuk
+    // mengembalikan nilainya (10.0f)
+    private static float simpel(){
+        return 10.0f;
+    }
+}
